b6be732f344661dceb0a316645e8c55ab3ff3b02
”Ivan <Trech06@mail.ru>
2021-05-15 18:22:06 +0300

    String reverse added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("new");
+        Scanner scanner = new Scanner(in);
+        String inputString = scanner.nextLine();
+        char[] charArray = inputString.toCharArray();
+        int inputStringLenght = inputString.length();
+        for (char c: charArray) {
+            out.print(c);
+        }
+        out.println();
+        out.println(inputStringLenght);
+        //  out.println("s");
+        for (int i = inputStringLenght; i > 0; i--)
+            out.print(charArray[i-1]);
+        out.println();
+        out.println("End of program");
     }
 }
